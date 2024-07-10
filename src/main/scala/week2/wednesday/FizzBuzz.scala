6a040ace8195ff2e7e483b2484c7b9e647a61436
Roshan Gautam <106965552+Roshangautam2052@users.noreply.github.com>
2024-07-10 16:28:06 +0100

    Fizz buzz (#4)
    * completed FizzBuzzTest
    
    * complete FizzBuzz
    
    * resolved the conflict

--- src/main/scala/week2/wednesday/FizzBuzz.scala
+++ src/main/scala/week2/wednesday/FizzBuzz.scala
@@ -1,5 +1,12 @@
 package week2.wednesday
 
-class FizzBuzz {
 
-}
+class FizzBuzz(var number: Int) {
+ def checkFizzBuzz(): String ={
+   if(number%3 ==0 && number%5 == 0) "FizzBuzz"
+   else if(number%3== 0) "Fizz"
+   else if(number%5== 0) "Buzz"
+   else number.toString
+ }
+
+
