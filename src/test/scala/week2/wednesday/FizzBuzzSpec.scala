323ae84b52b58a86ea5a73c4e55bc18661194fe8
Roshan Gautam <roshangautam2052@gmail.com>
2024-07-10 16:02:45 +0100

    complete FizzBuzz

--- src/test/scala/week2/wednesday/FizzBuzzSpec.scala
+++ src/test/scala/week2/wednesday/FizzBuzzSpec.scala
@@ -1,5 +1,30 @@
 package week2.wednesday
 
+import org.scalatest.FlatSpec
+
 class FizzBuzzSpec {
 
+class FizzBuzzSpec extends FlatSpec {
+  val testFizzBuzz = new FizzBuzz(3)
+  "checkFizzBuzz" should "check whether the function returns " +
+    "Fizz if divisible by 3 " in {
+    assert(
+        testFizzBuzz.checkFizzBuzz() == "Fizz"
+//        testFizzBuzz.checkFizzBuzz() == "Buzz" ||
+//        testFizzBuzz.checkFizzBuzz() == "FizzBuzz" ||
+//        testFizzBuzz.checkFizzBuzz() == testFizzBuzz.number.toString
+    )
+  }
+  "checkFizzBuzz" should "check whether the function returns " +
+    "Buzz if it is divisible by 5 " in {
+    assert(testFizzBuzz.checkFizzBuzz() == "Buzz")
+  }
+  "checkFizzBuzz" should "check whether the function returns " +
+    "FizzBuzz if divisible by 3 and 5 " in {
+    assert(testFizzBuzz.checkFizzBuzz() == "FizzBuzz")
+  }
+  "checkFizzBuzz" should "check whether the function returns " +
+    "string representation of the number if all above conditions are not met" in {
+    assert(testFizzBuzz.checkFizzBuzz() == testFizzBuzz.number.toString)
+  }
 }
