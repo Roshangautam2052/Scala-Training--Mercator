fa5c7fb0777fcc874054d62840f2a79df47f497e
Roshan Gautam <roshangautam2052@gmail.com>
2024-07-19 12:14:32 +0100

    completed lectrue

---
+++ src/main/scala/Week3/friday/Futures.scala
@@ -0,0 +1,21 @@
+package Week3.friday
+// global is a lazy val --> which is not executed until it is called
+import java.util.concurrent.TimeUnit
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.{Await, Future}
+import scala.concurrent.duration.{Duration, FiniteDuration}
+
+object Futures extends App{
+// They act as a placeholder
+
+  def addition(x:Int, y:Int) :Int ={
+    Thread.sleep(1000) // long computation time
+    x + y
+  }
+  def additionInFuture: Future[Int] = Future {
+    addition(100, 200)
+  }
+
+  val waitTime: FiniteDuration = Duration(5,TimeUnit.SECONDS)
+  println(Await.result(additionInFuture, waitTime))
+}
--- src/main/scala/Week3/thursday/afternoontask/extension/RunExtensionTask.scala
+++ src/main/scala/Week3/thursday/afternoontask/extension/RunExtensionTask.scala
@@ -1,29 +1,29 @@
-package Week3.thursday.afternoontask.extension
-
-object RunExtensionTask extends App {
-
-  // This is a valid letter
-  val letter: Either[GenericPostageError, Letter] = Right(Letter(Name("Michael"),PostCode("DE13 8ES")) )
-  // create a invalid letter
-
-  val invalidLetter:Either[GenericPostageError, Letter]= Left(InvalidNameError("Name is not valid "))
-  println(letter)
-  println(invalidLetter)
-
-  val letter1: Right[Nothing, Letter] = Right(Letter(Name("Michael"),PostCode("DE13 8ES")))
-  val invalidLetter2: Left[InvalidNameError, Nothing] = Left(InvalidNameError("The name is not valid"))
-
-  println(letter1)
-  println(invalidLetter2)
-
-  // maybePostCode
-  val postCode = PostCode
-  println(postCode.postCodeError("B70 8BS"))
-
-  // nameOrError
-  val name = Name
-  println(name.nameOrError(Name("test")))
-
-
-
-}
+//package Week3.thursday.afternoontask.extension
+//
+//object RunExtensionTask extends App {
+//
+//  // This is a valid letter
+//  val letter: Either[GenericPostageError, Letter] = Right(Letter(Name("Michael"),PostCode("DE13 8ES")) )
+//  // create a invalid letter
+//
+//  val invalidLetter:Either[GenericPostageError, Letter]= Left(InvalidNameError("Name is not valid "))
+//  println(letter)
+//  println(invalidLetter)
+//
+//  val letter1: Right[Nothing, Letter] = Right(Letter(Name("Michael"),PostCode("DE13 8ES")))
+//  val invalidLetter2: Left[InvalidNameError, Nothing] = Left(InvalidNameError("The name is not valid"))
+//
+//  println(letter1)
+//  println(invalidLetter2)
+//
+//  // maybePostCode
+//  val postCode = PostCode
+//  println(postCode.postCodeError("B70 8BS"))
+//
+//  // nameOrError
+//  val name = Name
+//  println(name.nameOrError(Name("test")))
+//
+//
+//
+//}
